76e8ad3beaa01ea180492015ff62f1dbf549d1a2
Abhishek Singh <117068155+Mercer011@users.noreply.github.com>
2023-11-09 00:13:06 +0530

    Add files via upload

--- MiniMusicPlayer.java
+++ MiniMusicPlayer.java
@@ -1,16 +1,29 @@
 import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 
-class Song {
-  private String title;
-  private String artist;
-  private int duration;
 
-  public Song(String title, String artist, int duration) {
-    this.title = title;
-    this.artist = artist;
-    this.duration = duration;
-  }
+class Song implements Serializable{
+    private String title;
+    private String artist;
+    private int duration;
+    private int index;
+
+  public Song(String title, String artist, int duration, int index) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.index = index;
+    }
+    public int getIndex() {
+        return index;
+    }
+
 
   public String toString() {
     return title + " by " + artist + " (" + duration + " seconds)";
@@ -18,10 +31,13 @@ class Song {
 }
 
 class Musicplayer {
-  private Queue<Song> listofsongs = new LinkedList<>();
-  private Queue<Song> queue = new LinkedList<>();
+  private List<Song> listofsongs = new LinkedList<>();
+  private List<Song> queue = new LinkedList<>();
+   private List<Integer> queueIndices = new ArrayList<>();
+
   private int SongIndex = -1;
   private String playMode = "Repeat all";
+  
 
   public void addsong(Song song) {
     listofsongs.add(song);
@@ -34,15 +50,55 @@ class Musicplayer {
       SongNumber++;
     }
   }
+   public void QueueAdd(int songIndex) {
+        if (queueIndices.contains(songIndex)) {
+            System.out.println("Song is already in the queue.");
+        } else {
+            queueIndices.add(songIndex);
+            System.out.println("Added song to the queue: " + listofsongs.get(songIndex).toString());
+        }
+    }
 
-public void NowPlaying() {
-  Song currentSong = queue.peek();
-  if (currentSong != null) {
-    System.out.println("Song playing: " + currentSong);
-  } else {
-    System.out.println("Currently no song is playing.");
+    public void QueueRemove(int songIndex) {
+        if (queueIndices.contains(songIndex)) {
+            queueIndices.remove(Integer.valueOf(songIndex));
+            System.out.println("Removed song from the queue: " + listofsongs.get(songIndex).toString());
+        } else {
+            System.out.println("Song not found in the queue.");
+        }
+    }
+
+
+    public void QueueEdit(String action, String value) {
+        if (action.equalsIgnoreCase("add")) {
+            List<Song> matchingSongs = SearchSongs(value);
+            if (!matchingSongs.isEmpty()) {
+                Song matchingSong = matchingSongs.get(0);
+                int songIndex = listofsongs.indexOf(matchingSong);
+                QueueAdd(songIndex);
+            } else {
+                System.out.println("Song not found.");
+            }
+        } else if (action.equalsIgnoreCase("remove")) {
+            List<Song> matchingSongs = SearchSongs(value);
+            if (!matchingSongs.isEmpty()) {
+                Song matchingSong = matchingSongs.get(0);
+                int songIndex = listofsongs.indexOf(matchingSong);
+                QueueRemove(songIndex);
+            } else {
+                System.out.println("Song not found in the queue.");
+            }
+        } else {
+            System.out.println("Invalid action. Use 'add' or 'remove'.");
+        }
+    }
+  public void NowPlaying() {
+    if (SongIndex >= 0 && SongIndex < queue.size()) {
+      System.out.println("Song playing " + queue.get(SongIndex));
+    } else {
+      System.out.println("Currently no song is playing ");
+    }
   }
-}
 
   public List<Song> SearchSongs(String keyword) {
     List<Song> MatchingSong = new LinkedList<>();
@@ -50,7 +106,6 @@ public void NowPlaying() {
       if (song.toString().toLowerCase().contains(keyword.toLowerCase())) {
         MatchingSong.add(song);
       }
-
     }
     return MatchingSong;
   }
@@ -68,39 +123,39 @@ public void NowPlaying() {
     }
   }
 
-public void QueueEdit(String action, String value) {
-  if (action.equals("ADD")) {
-    List<Song> matchingSongs = SearchSongs(value);
-    if (matchingSongs.isEmpty()) {
-      System.out.println("Songs not found to add to the queue");
-        } else {
-      queue.addAll(matchingSongs);
-      System.out.println("Added songs successfully.");
-    }
-  } else if (action.equals("REMOVE")) {
-    try {
-      int songNumber = Integer.parseInt(value);
-      if (songNumber >= 0 && songNumber <= queue.size()) {
-        Song removedSong = queue.poll();
-        System.out.println("Removed: " + removedSong);
-      } else {
-        System.out.println("Invalid input!");
-      }
-    } catch (Exception e) {
-      System.out.println("Enter a valid Song Number");
-    }
-  }
-}
+  // public void QueueEdit(String action, String value) {
+  //   if (action.equals("ADD")) {
+  //     List<Song> MatchingSong = SearchSongs(value);
+  //     if (MatchingSong.isEmpty()) {
+  //       System.out.println("Songs not found to add to the queue");
+  //     } else {
+  //       queue.addAll(MatchingSong);
+  //       System.out.println("\nAdded songs succesfully.");
+  //     }
+  //   } else if (action.equals("REMOVE")) {
+  //     try {
+  //       int SongNumber = Integer.parseInt(value);
+  //          System.out.println("SongNumber: " + SongNumber); 
+  //       if (SongNumber >= 0 && SongNumber < queue.size()) {
+  //         Song RemovedSong = queue.remove(SongNumber - 1);
+  //         System.out.println("Removed: " + RemovedSong);
+  //       } else {
+  //         System.out.println("Invalid input!");
+  //       }
+  //     } catch (Exception e) {
+  //       System.out.println("Enter a valid Song Number");
+  //     }
+  //   }
 
-public void ChangeMode(String mode) {
-  mode = mode.toLowerCase();
-  if (mode.equals("repeat one") || mode.equals("repeat all") || mode.equals("shuffle")) {
-    playMode = mode;
-    System.out.println("Play mode set to: " + mode);
-  } else {
-    System.out.println("Invalid playing mode, please choose between the available modes");
+
+  public void ChangeMode(String mode) {
+    if (mode.equals("Repeat the same Song") || mode.equals("Repeat All Songs") || mode.equals("Shuffle the playlist")) {
+      playMode = mode;
+      System.out.println("Play mode set to: " + mode);
+    } else {
+      System.out.println("Invalid playing mode,please choose Between the Available Modes");
+    }
   }
-}
 
   public void NextSong() {
     if (playMode.equals("Repeat the same Song")) {
@@ -118,12 +173,35 @@ public void ChangeMode(String mode) {
     }
     NowPlaying();
   }
+  public void QueueToFile(String filename ){
+    try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
+      outputStream.writeObject(queueIndices);
+      System.out.println("Queue saved to file: "+ filename);
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+   public void QueueFromFile(String filename) {
+    try {
+      ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename));{
+        queueIndices = (List<Integer>) inputStream.readObject();
+        System.out.println("Queue loaded from file: " + filename);
+      }
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+
+        }
+ 
 }
 
-public class MiniMusicPlayer {
+public static class MiniMusicPlayer {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     Musicplayer player = new Musicplayer();
+    player.QueueToFile("queue.ser");
+    player.QueueFromFile("queue.ser");
 
     while (true) {
       System.out.println("\nMENU:");
@@ -148,7 +226,7 @@ public class MiniMusicPlayer {
           String artist = sc.nextLine();
           System.out.print("Duration of the song: ");
           int duration = Integer.parseInt(sc.nextLine());
-          Song song = new Song(title, artist, duration);
+          Song song = new Song(title, artist, duration, duration);
           player.addsong(song);
           System.out.println(song + " has Been added to the Playlist");
           break;
@@ -210,4 +288,5 @@ public class MiniMusicPlayer {
     }
   }
 
+}
 }
